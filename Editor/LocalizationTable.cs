8f8fe5f5678f71f2dda5a7c215d304fb4ea8c04b
suzuryg <suzuryg@outlook.com>
2024-01-08 00:51:28 +0900

    feat: Add UI

---
+++ Editor/LocalizationTable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Suzuryg.LocalSwitchGenerator
+{
+    // [CreateAssetMenu(menuName = "LocalSwitchGenerator/LocalizationTable")]
+    internal class LocalizationTable : ScriptableObject
+    {
+        [Header("MainWindow_UI")]
+        public string TargetObject = "Target Object";
+        public string InitialState = "Initial State";
+        public string ToggleButtons = "Toggle ON and OFF Buttons";
+        public string Button = "Button";
+        public string OffToOn = "Button to Turn ON";
+        public string OnToOff = "Button to Turn OFF";
+        public string Generate = "Generate";
+        public string Cancel = "Cancel";
+
+        [Header("MainWindow_Help")]
+        public string ComponentsWillBeAdded = "Item component and InteractItemTrigger component will be added to the button.";
+
+        [Header("MainWindow_Message")]
+        public string PleaseSelectSceneObject = "Please select an object on the scene (you cannot select an asset).";
+        public string NoTarget = "Please select a target object.";
+        public string NoButtons = "Please select buttons.";
+        public string SameObjects = "Please select all different objects for the target object and the buttons.";
+        public string ConfirmGeneration = "Generate switch?";
+        public string GenerationCompleted = "Switch generation completed!";
+        public string GenerationError = "The following error occurred during switch generation. Please check the console for details.";
+        public string ComponentsAddedSingle = "Components have been added to \"<0>\".";
+        public string ComponentsAddedDouble = "Components have been added to \"<0>\" and \"<1>\".";
+        public string ObjectGenerated = "\"<0>\" object has been generated.";
+    }
+}
---
+++ Editor/MainWindow.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Suzuryg.LocalSwitchGenerator
+{
+    internal class MainWindow : EditorWindow
+    {
+        private static readonly float Margin = 5;
+        private static readonly float LabelWidth = 180;
+
+        private GameObject _targetObject = null;
+        private bool _defaultState = false;
+        private bool _toggleButtons = false;
+        private GameObject _singleSwitch = null;
+        private GameObject _offToOn = null;
+        private GameObject _onToOff = null;
+
+        private LocalizationSetting _loc;
+        private GUIStyle _radioButtonStyle;
+
+        private void OnEnable()
+        {
+            _loc = new LocalizationSetting();
+
+            titleContent = new GUIContent(Constants.SystemName);
+            minSize = new Vector2(435, 230);
+        }
+
+        private void OnGUI()
+        {
+            GetStyles();
+
+            // locale
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("言語設定 (Language Setting)");
+                var locale = (LocalizationSetting.Locale)EditorGUILayout.EnumPopup(string.Empty, _loc.CurrentLocale, GUILayout.Width(65));
+                if (locale != _loc.CurrentLocale)
+                {
+                    _loc.CurrentLocale = locale;
+                }
+                GUILayout.Space(10);
+            }
+
+            GUILayout.Space(Margin);
+
+            // target object
+            _targetObject = ObjectField(_targetObject, _loc.Table.TargetObject);
+
+            GUILayout.Space(Margin);
+
+            // initial state
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Label(_loc.Table.InitialState, GUILayout.Width(LabelWidth));
+                var options = new[] { new GUIContent("ON"), new GUIContent("OFF"), };
+                var index = _defaultState ? 0 : 1;
+                var newValue = GUILayout.SelectionGrid(index, options, xCount: 2, _radioButtonStyle);
+                if (newValue != index)
+                {
+                    _defaultState = !_defaultState;
+                }
+            }
+
+            GUILayout.Space(Margin);
+
+            // toggle buttons
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Label(_loc.Table.ToggleButtons, GUILayout.Width(LabelWidth));
+                _toggleButtons = GUILayout.Toggle(_toggleButtons, string.Empty);
+            }
+
+            // double switch
+            if (_toggleButtons)
+            {
+                _offToOn = ObjectField(_offToOn, _loc.Table.OffToOn);
+                _onToOff = ObjectField(_onToOff, _loc.Table.OnToOff);
+            }
+            // single switch
+            else
+            {
+                _singleSwitch = ObjectField(_singleSwitch, _loc.Table.Button);
+            }
+
+            EditorGUILayout.HelpBox(_loc.Table.ComponentsWillBeAdded, MessageType.Info);
+
+            GUILayout.FlexibleSpace();
+
+            // generate button
+            if (GUILayout.Button(_loc.Table.Generate, GUILayout.Height(30)))
+            {
+                var check = CheckArguments();
+                if (check.canExecute)
+                {
+                    if (EditorUtility.DisplayDialog(Constants.SystemName, _loc.Table.ConfirmGeneration, _loc.Table.Generate, _loc.Table.Cancel))
+                    {
+                        Generate();
+                    }
+                }
+                else
+                {
+                    EditorUtility.DisplayDialog(Constants.SystemName, check.message, "OK");
+                }
+            }
+
+            GUILayout.Space(15);
+        }
+
+        private void GetStyles()
+        {
+            if (_radioButtonStyle == null)
+            {
+                try
+                {
+                    _radioButtonStyle = new GUIStyle(EditorStyles.radioButton);
+                }
+                catch (NullReferenceException)
+                {
+                    _radioButtonStyle = new GUIStyle();
+                }
+                var padding = _radioButtonStyle.padding;
+                _radioButtonStyle.padding = new RectOffset(padding.left + 3, padding.right, padding.top, padding.bottom);
+            }
+        }
+
+        private GameObject ObjectField(GameObject exising, string label)
+        {
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Label(label, GUILayout.Width(LabelWidth));
+                var newValue = EditorGUILayout.ObjectField(string.Empty, exising, typeof(GameObject), allowSceneObjects: true) as GameObject;
+                if (newValue != null)
+                {
+                    if (newValue.scene.IsValid())
+                    {
+                        return newValue;
+                    }
+                    else
+                    {
+                        EditorUtility.DisplayDialog(Constants.SystemName, _loc.Table.PleaseSelectSceneObject, "OK");
+                        return exising;
+                    }
+                }
+                else
+                {
+                    return newValue;
+                }
+            }
+        }
+
+        private (bool canExecute, string message) CheckArguments()
+        {
+            if (_targetObject == null) { return (false, _loc.Table.NoTarget); }
+
+            // double switch
+            if (_toggleButtons)
+            {
+                if (_offToOn == null || _onToOff == null)
+                {
+                    return (false, _loc.Table.NoButtons);
+                }
+                else if (_targetObject == _offToOn || _targetObject == _onToOff || _offToOn == _onToOff)
+                {
+                    return (false, _loc.Table.SameObjects);
+                }
+                else
+                {
+                    return (true, string.Empty);
+                }
+            }
+            // single switch
+            else
+            {
+                if (_singleSwitch == null)
+                {
+                    return (false, _loc.Table.NoButtons);
+                }
+                else if (_targetObject == _singleSwitch)
+                {
+                    return (false, _loc.Table.SameObjects);
+                }
+                else
+                {
+                    return (true, string.Empty);
+                }
+            }
+        }
+
+        private void Generate()
+        {
+            try
+            {
+                var switchGenerator = new SwitchGenerator(Guid.NewGuid().ToString("N"), _targetObject);
+                while (AssetDatabase.IsValidFolder(switchGenerator.AssetDir))
+                {
+                    switchGenerator = new SwitchGenerator(Guid.NewGuid().ToString("N"), _targetObject);
+                }
+
+                var message = new StringBuilder();
+                message.AppendLine(_loc.Table.GenerationCompleted).AppendLine();
+
+                // double switch
+                if (_toggleButtons)
+                {
+                    switchGenerator.Generate(_offToOn, _onToOff, _defaultState);
+                    message.AppendLine(_loc.Table.ComponentsAddedDouble.Replace("<0>", _offToOn.name).Replace("<1>", _onToOff.name)).AppendLine();
+                }
+                // single switch
+                else
+                {
+                    switchGenerator.Generate(_singleSwitch, _defaultState);
+                    message.AppendLine(_loc.Table.ComponentsAddedSingle.Replace("<0>", _singleSwitch.name)).AppendLine();
+                }
+
+                message.AppendLine(_loc.Table.ObjectGenerated.Replace("<0>", switchGenerator.RootObjectName)).AppendLine();
+                EditorUtility.DisplayDialog(Constants.SystemName, message.ToString(), "OK");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                EditorUtility.DisplayDialog(Constants.SystemName, _loc.Table.GenerationError + "\n\n" + ex.Message, "OK");
+            }
+        }
+
+        [MenuItem("Tools/LocalSwitchGenerator")]
+        private static void Open() => GetWindow<MainWindow>();
+    }
+}
